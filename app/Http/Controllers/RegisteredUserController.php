af1d9f5fcb93f063d4c77473829f2072bda5419e
Fernandes <luca@quantumtheorem.com.br>
2026-06-18 08:58:23 -0300

    feat: included alpinejs

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -19,7 +19,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'min:3', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users', 'email')],
-            'password' => ['required'. 'string', 'min:8', 'max:255'],
+            'password' => ['required', 'string', 'min:8', 'max:255'],
         ]);
 
         $user = User::create([
--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -13,5 +13,16 @@
     <main class="max-w-7xl mx-auto px-6 pb-10">
         {{ $slot }}
     </main>
+
+    @session('success')
+        <div
+            x-data="{ show: true }" 
+            x-show="show"
+            x-init="setTimeout(() => show = false, 3000)"
+            x-transition.opacity.duration.300ms
+            class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg">
+            {{ $value }}
+        </div>
+    @endsession
 </body>
 </html>
